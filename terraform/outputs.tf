5492a0acc1270d1edc90e04a60eaf23b79715194
Devalla Lavanya <lavanyadevalla1@gmail.com>
2025-12-03 23:52:04 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,10 +6,6 @@ output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "cluster_ca_certificate" {
+output "cluster_certificate_authority_data" {
   value = module.eks.cluster_certificate_authority_data
-}
-
-output "kubeconfig" {
-  value = module.eks.kubeconfig
 }
